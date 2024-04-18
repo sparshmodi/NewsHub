aac5b272d6df41f86f6b909aaf22f17d476bf35d
Sparsh Modi <sparshmodi2016@gmail.com>
2024-04-17 21:16:17 -0400

    Initial Commit

---
+++ NewsHub/NewsHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsHubApp.swift
+//  NewsHub
+//
+//  Created by Sparsh Modi on 17-04-2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
